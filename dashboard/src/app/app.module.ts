ba0ea0844926ecdaae6b1066bed549f78184fc2b
Ali Umar <ali.umar.work@gmail.com>
2022-07-23 18:15:48 -0400

    initial landing page setup

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
